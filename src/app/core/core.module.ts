4f65a1df18e9789478fb66938ac05899e3941b1a
doug0102 <shea.wow@hotmail.com>
2022-11-17 14:24:54 -0500

    basic theme

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthConfigModule } from './auth/auth-config/auth-config.module';
+import { AuthConfigModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [],
