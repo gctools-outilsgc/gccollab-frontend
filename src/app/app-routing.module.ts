bfcf806ec4901d03c62b57377cc2b8c42f43a01d
doug0102 <shea.wow@hotmail.com>
2023-02-01 11:55:02 -0500

    title component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,50 +13,74 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: CoreRoutes.Home,
+    redirectTo: CoreRoutes.Home
   },
   {
     path: CoreRoutes.Login,
     title: 'titles.login',
     loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule),
-    canActivate: [LoginGuard]
+    canActivate: [LoginGuard],
+    data: {
+      breadcrumb: 'titles.login'
+    }
   },
   {
     path: CoreRoutes.Home,
     title: 'titles.home',
     loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.home'
+    }
   },
   {
     path: CoreRoutes.Register,
     title: 'titles.register',
     loadChildren: () => import('./features/register/register.module').then(m => m.RegisterModule),
     canActivate: [LoginGuard],
+    data: {
+      breadcrumb: 'titles.register'
+    }
   },
   {
     path: CoreRoutes.Terms,
     title: 'titles.terms',
     loadChildren: () => import('./features/terms/terms.module').then(m => m.TermsModule),
+    data: {
+      breadcrumb: 'titles.terms'
+    }
   },
   {
     path: CoreRoutes.Splash,
     title: 'titles.splash', 
     loadChildren: () => import('./features/splash/splash.module').then(m => m.SplashModule),
+    data: {
+      breadcrumb: 'titles.splash'
+    }
   },
   {
     path: CoreRoutes.About,
     title: 'titles.about', 
-    loadChildren: () => import('./features/about/about.module').then(m => m.AboutModule)
+    loadChildren: () => import('./features/about/about.module').then(m => m.AboutModule),
+    data: {
+      breadcrumb: 'titles.about'
+    }
   },
   {
     path: CoreRoutes.Privacy,
     title: 'titles.privacy',
-    loadChildren: () => import('./features/privacy/privacy.module').then(m => m.PrivacyModule)
+    loadChildren: () => import('./features/privacy/privacy.module').then(m => m.PrivacyModule),
+    data: {
+      breadcrumb: 'titles.privacy'
+    }
   },
   {
     path: CoreRoutes.Stats,
     title: 'titles.stats',
-    loadChildren: () => import('./features/stats/stats.module').then(m => m.StatsModule)
+    loadChildren: () => import('./features/stats/stats.module').then(m => m.StatsModule),
+    data: {
+      breadcrumb: 'titles.stats'
+    }
   },
   {
     path: CoreRoutes.Help,
@@ -71,85 +95,127 @@ const routes: Routes = [
     path: CoreRoutes.Blog,
     title: 'titles.blog',
     loadChildren: () => import('./features/blog/blog.module').then(m => m.BlogModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.blog'
+    }
   },
   {
     path: CoreRoutes.Bookmarks,
     title: 'titles.bookmarks',
     loadChildren: () => import('./features/bookmarks/bookmarks.module').then(m => m.BookmarksModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.bookmarks'
+    }
   },
   {
     path: CoreRoutes.Dashboard,
     title: 'titles.dashboard',
     loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.dashboard'
+    }
   },
   {
     path: CoreRoutes.Friends,
     title: 'titles.friends',
     loadChildren: () => import('./features/friends/friends.module').then(m => m.FriendsModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.friends'
+    }
   },
   {
     path: CoreRoutes.Groups,
     title: 'titles.groups',
     loadChildren: () => import('./features/groups/groups.module').then(m => m.GroupsModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.groups'
+    }
   },
   {
     path: CoreRoutes.Invite,
     title: 'titles.invite',
     loadChildren: () => import('./features/invite/invite.module').then(m => m.InviteModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.invite'
+    }
   },
   {
     path: CoreRoutes.Members,
     title: 'titles.members',
     loadChildren: () => import('./features/members/members.module').then(m => m.MembersModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.members'
+    }
   },
   {
     path: CoreRoutes.Messages,
     title: 'titles.messages',
     loadChildren: () => import('./features/messages/messages.module').then(m => m.MessagesModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.messages'
+    }
   },
   {
     path: CoreRoutes.Missions,
     title: 'titles.missions',
     loadChildren: () => import('./features/missions/missions.module').then(m => m.MissionsModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.missions'
+    }
   },
   {
     path: CoreRoutes.NewsFeed,
     title: 'titles.newsfeed',
     loadChildren: () => import('./features/news-feed/news-feed.module').then(m => m.NewsFeedModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.newsfeed'
+    }
   },
   {
     path: CoreRoutes.Profile,
     title: 'titles.profile',
     loadChildren: () => import('./features/profile/profile.module').then(m => m.ProfileModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.profile'
+    }
   },
   {
     path: CoreRoutes.Search,
     title: 'titles.search',
     loadChildren: () => import('./features/search/search.module').then(m => m.SearchModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.search'
+    }
   },
   {
     path: CoreRoutes.Settings,
     title: 'titles.settings',
     loadChildren: () => import('./features/settings/settings.module').then(m => m.SettingsModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.settings'
+    }
   },
   {
     path: CoreRoutes.TheWire,
     title: 'titles.thewire',
     loadChildren: () => import('./features/the-wire/the-wire.module').then(m => m.TheWireModule),
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
+    data: {
+      breadcrumb: 'titles.thewire'
+    }
   },
   {
     path: CoreRoutes.Unauthorized,
--- src/app/core/components/page-title/page-title.component.ts
+++ src/app/core/components/page-title/page-title.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-page-title',
@@ -7,11 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageTitleComponent implements OnInit {
 
-  // TODO: Get current page/breadcrumbs
+  // TODO: Change to breadcrumb
+  public title: string[] = [];
   
-  constructor() {  }
+  constructor(public router: Router, private activatedRoute: ActivatedRoute) {  }
 
   ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => this.title = this.getTitle(this.activatedRoute.root));
   }
 
+  private getTitle(route: ActivatedRoute): string[] {
+    const children: ActivatedRoute[] = route.children;
+
+    if (children.length === 0) {
+      return [];
+    }
+
+    let title: string[] = [];
+    for (const child of children) {
+
+      const label = child.snapshot.data["breadcrumb"];
+
+      if (label != undefined && label != null) {
+        title.push(label);
+      }
+    }
+
+    return title;
+  }
 }
