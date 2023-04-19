cdce567d1950b740b89e74ae9c942a372528f09f
doug0102 <shea.wow@hotmail.com>
2023-04-19 15:25:51 -0400

    change editor language dynamically

--- src/app/shared/components/editor/editor.component.ts
+++ src/app/shared/components/editor/editor.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { Editor } from 'ngx-editor';
+import { Editor, NgxEditorService, Toolbar } from 'ngx-editor';
+import { ngxEditorLocals } from '../../factories/editor-config.factory';
+import { Subscription } from 'rxjs/internal/Subscription';
 
+// https://sibiraj-s.github.io/ngx-editor/en/introduction/
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
@@ -9,10 +12,27 @@ import { Editor } from 'ngx-editor';
 })
 export class EditorComponent implements OnInit, OnDestroy {
   editor: Editor;
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [ { heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+    ['horizontal_rule', 'format_clear'],
+  ]
   html: string = '';
 
-  constructor(private translateService: TranslateService) {
+  private langChangeSub!: Subscription;
+
+  constructor(private translateService: TranslateService, private ngxEditorService: NgxEditorService ) {
     this.editor = new Editor();
+
+    this.langChangeSub = this.translateService.onLangChange.subscribe(() => {
+      this.onLangChange();
+    });
    }
 
   ngOnInit(): void {
@@ -23,6 +43,13 @@ export class EditorComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     if (this.editor)
       this.editor.destroy();
+
+    if (this.langChangeSub)
+      this.langChangeSub.unsubscribe();
+  }
+
+  onLangChange(): void {
+    this.ngxEditorService.config.locals = ngxEditorLocals(this.translateService);
   }
 
   placeholderText(): string {
