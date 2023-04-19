32f9eb7c080c386bf035642d86a6e701223a5b7e
doug0102 <shea.wow@hotmail.com>
2023-04-19 15:38:39 -0400

    passing test for editor.component.spec.ts

--- src/app/shared/components/editor/editor.component.spec.ts
+++ src/app/shared/components/editor/editor.component.spec.ts
@@ -2,16 +2,37 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditorComponent } from './editor.component';
 
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpLoaderFactory } from 'src/app/app.module';
+import { TranslateService } from '@ngx-translate/core';
+
 describe('EditorComponent', () => {
   let component: EditorComponent;
   let fixture: ComponentFixture<EditorComponent>;
+  let translateService: TranslateService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditorComponent ]
+      declarations: [ EditorComponent ],
+      imports: [ 
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [ HttpClient ]
+          }
+        }),
+        HttpClientModule
+      ],
+      providers: [
+        TranslateService, 
+        HttpClient,
+      ]
     })
     .compileComponents();
 
+    translateService = TestBed.inject(TranslateService);
     fixture = TestBed.createComponent(EditorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
