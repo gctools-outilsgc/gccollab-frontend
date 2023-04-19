46548d89e7413e5ad1795e978084935b4375bbc3
doug0102 <shea.wow@hotmail.com>
2023-04-19 12:43:13 -0400

    translate placeholder text, fix drop down menu style

--- src/app/shared/components/editor/editor.component.ts
+++ src/app/shared/components/editor/editor.component.ts
@@ -1,17 +1,17 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { Editor } from 'ngx-editor';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./editor.component.scss']
 })
 export class EditorComponent implements OnInit, OnDestroy {
   editor: Editor;
   html: string = '';
 
-  constructor() {
+  constructor(private translateService: TranslateService) {
     this.editor = new Editor();
    }
 
@@ -23,5 +23,9 @@ export class EditorComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     if (this.editor)
       this.editor.destroy();
+  }
+
+  placeholderText(): string {
+    return this.translateService.instant('editor.placeholder')
   }
 }
