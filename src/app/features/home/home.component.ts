55ae0fb0b4d55d85aced2356714afd4cb146df21
doug0102 <shea.wow@hotmail.com>
2023-04-26 09:52:08 -0400

    clean up home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,23 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Translations } from 'src/app/core/services/translations.service';
-import { ButtonType } from 'src/app/shared/models/button-type';
-import { MaterialButtonType } from 'src/app/shared/models/material-button-type';
-import { MaterialColor } from 'src/app/shared/models/material-color';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  public buttonTypes = ButtonType;
-  public matButtonTypes = MaterialButtonType;
-  public matColors = MaterialColor;
-
-  public btnClick() {
-    console.log("Button Clicked");
-  }
 
   constructor(public translations: Translations) { }
 
