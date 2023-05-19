152256297080ac99ae75f31ef8b76c68de2a3ad3
doug0102 <shea.wow@hotmail.com>
2023-05-19 09:46:50 -0400

    fixed news-card test

--- src/app/features/news-feed/components/news-card/news-card.component.spec.ts
+++ src/app/features/news-feed/components/news-card/news-card.component.spec.ts
@@ -1,15 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewsCardComponent } from './news-card.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TypescriptLoader } from 'src/app/core/helpers/typescript-loader';
 
 describe('NewsCardComponent', () => {
   let component: NewsCardComponent;
+  let translateService: TranslateService;
   let fixture: ComponentFixture<NewsCardComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [NewsCardComponent]
+      declarations: [NewsCardComponent],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (http: HttpClient) => new TypescriptLoader(http, 'translations'),
+            deps: [ HttpClient ]
+          }
+        }),
+        HttpClientModule
+      ],
+      providers: [ TranslateService, HttpClient ]
     });
+  });
+
+  beforeEach(() => {
+    translateService = TestBed.inject(TranslateService);
     fixture = TestBed.createComponent(NewsCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
