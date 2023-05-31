383b2375567f9e3c31edd993083fbbbd5bf04923
doug0102 <shea.wow@hotmail.com>
2023-05-31 11:51:27 -0400

    event card test

--- src/app/features/events/components/event-card/event-card.component.spec.ts
+++ src/app/features/events/components/event-card/event-card.component.spec.ts
@@ -1,15 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EventCardComponent } from './event-card.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TypescriptLoader } from 'src/app/core/helpers/typescript-loader';
 
 describe('EventCardComponent', () => {
   let component: EventCardComponent;
+  let translateService: TranslateService;
   let fixture: ComponentFixture<EventCardComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EventCardComponent]
+      declarations: [EventCardComponent],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (http: HttpClient) => new TypescriptLoader(http, 'translations'),
+            deps: [ HttpClient ]
+          }
+        }),
+        HttpClientModule
+      ],
+      providers: [ TranslateService, HttpClient ]
     });
+  });
+
+  beforeEach(() => {
+    translateService = TestBed.inject(TranslateService);
     fixture = TestBed.createComponent(EventCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
