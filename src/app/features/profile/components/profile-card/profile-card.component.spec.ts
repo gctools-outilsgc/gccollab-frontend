515f86766bdbcc9017a62fb6c0949f6881a2409b
doug0102 <shea.wow@hotmail.com>
2023-06-02 13:47:01 -0400

    fix tests

--- src/app/features/profile/components/profile-card/profile-card.component.spec.ts
+++ src/app/features/profile/components/profile-card/profile-card.component.spec.ts
@@ -1,15 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileCardComponent } from './profile-card.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TypescriptLoader } from 'src/app/core/helpers/typescript-loader';
 
 describe('ProfileCardComponent', () => {
   let component: ProfileCardComponent;
+  let translateService: TranslateService;
   let fixture: ComponentFixture<ProfileCardComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ProfileCardComponent]
+      declarations: [ProfileCardComponent],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (http: HttpClient) => new TypescriptLoader(http, 'translations'),
+            deps: [ HttpClient ]
+          }
+        }),
+        HttpClientModule
+      ],
+      providers: [ TranslateService, HttpClient ]
     });
+  });
+
+  beforeEach(() => {
+    translateService = TestBed.inject(TranslateService);
     fixture = TestBed.createComponent(ProfileCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
