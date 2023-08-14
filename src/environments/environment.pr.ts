e9e28c0fc37e6ffc2bcffb184cfdc001ed5d31cc
doug0102 <shea.wow@hotmail.com>
2023-08-14 15:29:43 -0400

    improved workflow for pr

---
+++ src/environments/environment.pr.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    clientId: "429862", // TODO: Create client during workflow and insert here
+    baseUrl: "https://dev.account.gccollab.ca/api",
+    authUrl: "https://dev.account.gccollab.ca/openid",
+    authLogLevel: 0,
+    i18nFolder: "./assets/i18n/"
+  };
