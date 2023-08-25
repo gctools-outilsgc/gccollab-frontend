2802f1cc24f2a92a19a6a71135d44990f9b70f64
doug0102 <shea.wow@hotmail.com>
2023-08-25 12:44:50 -0400

    fix tests

--- src/app/shared/components/calendar-button/calendar-button.component.spec.ts
+++ src/app/shared/components/calendar-button/calendar-button.component.spec.ts
@@ -1,15 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalendarButtonComponent } from './calendar-button.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TypescriptLoader } from 'src/app/core/helpers/typescript-loader';
 
 describe('CalendarButtonComponent', () => {
   let component: CalendarButtonComponent;
+  let translateService: TranslateService;
   let fixture: ComponentFixture<CalendarButtonComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CalendarButtonComponent]
+      declarations: [CalendarButtonComponent],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (http: HttpClient) => new TypescriptLoader(http, 'translations'),
+            deps: [ HttpClient ]
+          }
+        }),
+        HttpClientModule
+      ],
+      providers: [ TranslateService, HttpClient ]
     });
+  });
+
+  beforeEach(() => {
+    translateService = TestBed.inject(TranslateService);
     fixture = TestBed.createComponent(CalendarButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
