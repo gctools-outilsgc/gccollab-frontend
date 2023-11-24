ef1fa436a649ec3b6c27c35969e363742724474c
doug0102 <shea.wow@hotmail.com>
2023-11-24 14:26:35 -0500

    wip list paging

--- src/app/shared/components/list/list.component.ts
+++ src/app/shared/components/list/list.component.ts
@@ -13,11 +13,18 @@ export class ListComponent implements OnInit {
   @Input() items: typeof this.service.dataType[] = [];
   @Input() cardSize: CardSize | string = CardSize.Small; // TODO: Card size on all card components. Make a base component or interface that gets implemented.
   @Input() orientation: Orientation | string = Orientation.Vertical;
-  @Input() loadItems: number = 3;
-  @Input() gap: number = 40;
+  @Input() pageSize: number = 10;
+  @Input() loadTime: number = 5000;
+  @Input() columnGap: number = 10;
+  @Input() rowGap: number = 40;
 
+  currentPage: number = 1;
+  lastPage: number = this.currentPage;
   loading: boolean = true;
   Orientations = Orientation;
+
+  nextPageCallback: Function = this.nextPage.bind(this);
+  previousPageCallback: Function = this.previousPage.bind(this);
 
   constructor() {
     
@@ -25,11 +32,43 @@ export class ListComponent implements OnInit {
   
   ngOnInit(): void { 
     if (this.items.length === 0) {
-      this.service?.getMany(10, 5000).subscribe((items: typeof this.service.dataType[]) => {
-        this.items = items;
-        this.loading = false;
-      });
+      this.loadNext();
     }
+  }
+
+  loadNext(): void {
+    this.loading = true;
+
+    this.service?.getMany(this.pageSize, this.loadTime).subscribe((items: typeof this.service.dataType[]) => {
+      this.items.push(...items);
+      this.loading = false;
+    });
+  }
+
+  nextPage(): void {
+    this.lastPage = ++this.currentPage;
+
+    if (this.pageSize * this.currentPage > this.items.length)
+      this.loadNext();
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1)
+      this.currentPage--;
+  }
+
+  get startIndex(): number {
+    return (this.currentPage - 1) * this.pageSize;
+  }
+
+  get endIndex(): number {
+    return this.startIndex + this.pageSize;
+  }
+
+  get paginatedItems(): any[] {
+    return this.loading && this.currentPage === this.lastPage 
+    ? this.items.slice(0, this.pageSize) 
+    : this.items.slice(this.startIndex, this.endIndex);
   }
 }
 
