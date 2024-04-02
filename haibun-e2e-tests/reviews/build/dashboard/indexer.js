18d89d57db64511b12a5ef5094fe9bbc494fd3a1
doug0102 <doug0102@users.noreply.github.com>
2024-04-02 17:54:22 +0000

    update e2e test reviews

--- haibun-e2e-tests/reviews/build/dashboard/indexer.js
+++ haibun-e2e-tests/reviews/build/dashboard/indexer.js
@@ -1,2 +1,2 @@
 export const endpoint = "./tracks/";
-export async function getPublishedReviews() { return ["1710878976452-tracksHistory.json","1710967231837-tracksHistory.json","1712079967005-tracksHistory.json"]; }
+export async function getPublishedReviews() { return ["1710967231837-tracksHistory.json","1712079967005-tracksHistory.json","1712080458089-tracksHistory.json"]; }
