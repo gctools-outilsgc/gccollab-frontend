491375c9ceee5f513f15330cce84dd81b8b64ddd
GCCollabBot <GCCollabBot@users.noreply.github.com>
2024-04-02 17:46:12 +0000

    update e2e test reviews

--- haibun-e2e-tests/reviews/build/dashboard/indexer.js
+++ haibun-e2e-tests/reviews/build/dashboard/indexer.js
@@ -1,2 +1,2 @@
 export const endpoint = "./tracks/";
-export async function getPublishedReviews() { return ["1710877689685-tracksHistory.json","1710878976452-tracksHistory.json","1710967231837-tracksHistory.json"]; }
+export async function getPublishedReviews() { return ["1710878976452-tracksHistory.json","1710967231837-tracksHistory.json","1712079967005-tracksHistory.json"]; }
