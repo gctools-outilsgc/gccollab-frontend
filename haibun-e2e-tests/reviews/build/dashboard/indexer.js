de2d6d00f70b7fa3ff9284eafed15702852606a2
AdiMakkar <AdiMakkar@users.noreply.github.com>
2024-04-03 14:54:02 +0000

    update e2e test reviews

--- haibun-e2e-tests/reviews/build/dashboard/indexer.js
+++ haibun-e2e-tests/reviews/build/dashboard/indexer.js
@@ -1,2 +1,2 @@
 export const endpoint = "./tracks/";
-export async function getPublishedReviews() { return ["1710967231837-tracksHistory.json","1712079967005-tracksHistory.json","1712080458089-tracksHistory.json"]; }
+export async function getPublishedReviews() { return ["1712079967005-tracksHistory.json","1712080458089-tracksHistory.json","1712156035339-tracksHistory.json"]; }
